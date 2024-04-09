c4e90ccfc7a3c85000f6165e791fde745db3a66e
Bryan Edward <bryanedwardse@gmail.com>
2024-04-10 01:07:53 +0200

    Playwright test

---
+++ test/.test.js
@@ -0,0 +1,16 @@
+// Cargar las variables de entorno
+require('dotenv').config();
+
+const { test, expect } = require('@playwright/test');
+
+test('Prueba de Playwright con variables de entorno', async ({ page }) => {
+    // Acceder a la variable de entorno BASE_URL
+    const baseUrl = process.env.BASE_URL;
+
+    // Realizar alguna acción en la página web utilizando Playwright
+    await page.goto(baseUrl);
+
+    // Verificar alguna condición en la página
+    const title = await page.title();
+    expect(title).toBe('Título esperado de la página');
+});
