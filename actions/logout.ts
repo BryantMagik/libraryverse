ca550b1a40cf9dab1dee9b38eb450dfe0fc2d938
beca_utd <gitlab.utd@linguaserve.com>
2024-04-16 09:38:54 +0200

    Deploy

--- actions/logout.ts
+++ actions/logout.ts
@@ -1,6 +1,8 @@
-"use server"
-import { signOut } from "@/auth"
+"use server";
+
+import { signOut } from "@/auth";
 
 export const logout = async () => {
-    await signOut()
-}
+    await signOut({redirectTo: "/" });
+
+};
