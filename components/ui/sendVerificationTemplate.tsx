f919ae90243f3c1adedace4bc3e3544219df0c7e
beca_utd <gitlab.utd@linguaserve.com>
2024-04-29 10:50:29 +0200

    Deployment try: v2

--- components/ui/sendVerificationTemplate.tsx
+++ components/ui/sendVerificationTemplate.tsx
@@ -5,21 +5,16 @@ import Head from "next/head"
 export default function VerificationEmailTemplate(confirmLink: string) {
 
     return (
-        <html lang="en">
-            <Head children={undefined}>
-            </Head>
-            <body>
-                <div className="container">
-                    <h1>Welcome to Next-Auth V5</h1>
-                    <p>Please confirm your email by clicking here!</p>
-                    <p>
-                        <a className="button" href={confirmLink}>
-                            Confirm your email
-                        </a>
-                    </p>
-                </div>
-            </body>
-        </html>
+        <body>
+            <div className="container">
+                <h1>Welcome to Next-Auth V5</h1>
+                <p>Please confirm your email by clicking here!</p>
+                <p>
+                    <a className="button" href={confirmLink}>
+                        Confirm your email
+                    </a>
+                </p>
+            </div>
+        </body>
     )
-
 }
