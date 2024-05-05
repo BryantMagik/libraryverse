6c44dc4b44d5d83d54dc4e19dbbb46c0d0b1a176
Bryan Edward <bryanedwardse@gmail.com>
2024-05-05 21:35:56 +0200

    Fixed

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,24 @@
 "use client"
-import Loading from "@/components/home/loading-homepage";
-import NavbarHome from '@/components/home/navbar-homepage';
-import { useState } from "react";
-//todo//
+import Loading from "@/components/home/loading-homepage"
+import NavbarHome from '@/components/home/navbar-homepage'
+import Image from 'next/image'
+import { useState } from "react"
 export default function Home() {
   const [loadingComplete, setLoadingComplete] = useState(false);
 
   return (
     <div >
-      {/* <Image className="relative" src={"/home/home1.webp"} alt={""}
-        width={1200}
-        height={800}
-      /> */}
       <Loading onLoadingComplete={() => setLoadingComplete(true)} />
-      {loadingComplete && <NavbarHome />}
+      {loadingComplete &&
+        <div>
+          <NavbarHome />
+          {/* <Image className="relative" src={"/home/home1.webp"} alt={""}
+            width={1200}
+            height={800}
+          /> */}
+        </div>
+      }
+
     </div>
   )
 }
--- components/home/navbar-homepage.tsx
+++ components/home/navbar-homepage.tsx
@@ -1,23 +1,19 @@
 "use client"
-
-import { AuthButton } from "@/components/auth/auth-button"
-import { Button } from "@/components/ui/button"
-import { IoPersonCircleOutline } from "react-icons/io5"
+import { AuthButton } from "@/components/auth/auth-button";
+import { Button } from "@/components/ui/button";
 
 export default function NavbarHome() {
-
     return (
-        <nav className="flex w-full h-20 px-4 text-white bg-almond-100 justify-center">
-            <div>
-                LibraryVerse
+        <nav className="flex flex-col lg:flex-row items-center justify-between w-full h-20 px-4 text-white bg-almond-100">
+            <div className="text-lg font-bold">LibraryVerse</div>
+            <div className="flex flex-col lg:flex-row items-center mt-4 lg:mt-0">
+                <AuthButton mode="modal" formType="login" asChild>
+                    <Button className="rounded-full bg-almond-500 text-white hover:bg-almond-700 lg:mr-4" size="lg">Iniciar Sesión</Button>
+                </AuthButton>
+                <AuthButton mode="modal" formType="register" asChild>
+                    <Button className="rounded-full bg-almond-500 text-white hover:bg-almond-700" size="lg">Registro</Button>
+                </AuthButton>
             </div>
-
-            <AuthButton mode="modal" formType={"login"} asChild >
-                <Button className="rounded-full bg-almond-500 text-white hover:bg-almond-700" size="lg">Inciar Sesión</Button>
-            </AuthButton>
-            <AuthButton mode="modal" formType={"register"} asChild >
-                <Button className="rounded-full bg-almond-500 text-white hover:bg-almond-700" size="lg">Registro</Button>
-            </AuthButton>
         </nav>
     )
 }
