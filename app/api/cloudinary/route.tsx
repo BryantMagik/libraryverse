814dc88a84ea688e06a399082759eb544d3b761d
Bryan Edward <bryanedwardse@gmail.com>
2024-05-16 20:12:26 +0200

    fixed env

--- app/api/cloudinary/route.tsx
+++ app/api/cloudinary/route.tsx
@@ -1,17 +1,20 @@
 import { v2 as cloudinary } from 'cloudinary'
 
 cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET,
-  })
-  
+  cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+  secure: true,
+})
+
+export default cloudinary
+
 export async function POST(request: Request) {
-    const body = (await request.json()) as { paramsToSign: Record<string, string> }
-    const { paramsToSign } = body
+  const body = (await request.json()) as { paramsToSign: Record<string, string> }
+  const { paramsToSign } = body
 
-    const signature = cloudinary.utils.api_sign_request(paramsToSign, process.env.CLOUDINARY_API_SECRET as string)
+  const signature = cloudinary.utils.api_sign_request(paramsToSign, process.env.CLOUDINARY_API_SECRET as string)
 
-    return Response.json({ signature })
+  return Response.json({ signature })
 
 }
